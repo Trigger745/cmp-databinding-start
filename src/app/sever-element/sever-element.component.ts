018b7e430c8a40f1ce3f1735a64b8ce50f0173cd
Trigger745 <Triggeryeet5@gmail.com>
2022-11-01 14:00:02 -0500

    Initial commit

---
+++ src/app/sever-element/sever-element.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-sever-element',
+  templateUrl: './sever-element.component.html',
+  styleUrls: ['./sever-element.component.css']
+})
+export class SeverElementComponent implements OnInit {
+  @Input() element: {type: string, name: string, content: string };
+  constructor() { }
+
+  ngOnInit(): void  {
+
+  }
+
+}
